ce3aa0c11e19860fd4554658e6266a3b764c77e0
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-05-21 09:25:05 -0600

    add tech boxes

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,15 +1,115 @@
-export default function TechStack(){
-    return(
-        <>
-        <div className="tech-container">
-            <h2>Tech Stack</h2>
+export default function TechStack() {
+  return (
+    <>
+      <div className="tech-container">
+        <h2>Tech Stack</h2>
 
-            <h4>Current Tech</h4>
-            <h4>Currently Learning</h4>
-            {/* TypeScript, Go, Solidity */}
-            <h4>Experience With</h4>
-            {/* E.g. Figma, Shopify, WordPress, Contentful, etc. */}
+        {/* ❗ Turn into a dictionary? And loop thru to create these divs with name as key and img as value? */}
+        <h4>Current Tech</h4>
+        <div className="tech-box">
+          <img src="tbd" />
+          <p>JavaScript</p>
         </div>
-        </>
-    )
+        <div className="tech-box">
+          <img src="tbd" />
+          <p>TypeScript</p>
+        </div>
+        <div className="tech-box">
+          <img src="tbd" />
+          <p>React</p>
+        </div>
+        <div className="tech-box">
+          <img src="tbd" />
+          <p>Python</p>
+        </div>
+        <div className="tech-box">
+          <img src="tbd" />
+          <p>Flask</p>
+        </div>
+        <div className="tech-box">
+          <img src="tbd" />
+          <p>SQLAlchemy</p>
+        </div>
+        <div className="tech-box">
+          <img src="tbd" />
+          <p>Postgres</p>
+        </div>
+        <div className="tech-box">
+          <img src="tbd" />
+          <p>Supbase</p>
+        </div>
+        <div className="tech-box">
+          <img src="tbd" />
+          <p>Vite</p>
+        </div>
+        <div className="tech-box">
+          <img src="tbd" />
+          <p>HTML</p>
+        </div>
+        <div className="tech-box">
+          <img src="tbd" />
+          <p>CSS</p>
+        </div>
+        <div className="tech-box">
+          <img src="tbd" />
+          <p>Tailwind</p>
+        </div>
+        <div className="tech-box">
+          <img src="tbd" />
+          <p>Bootstrap</p>
+        </div>
+        <div className="tech-box">
+          <img src="tbd" />
+          <p>Auth0</p>
+        </div>
+
+        <h4>Currently Learning</h4>
+        <div className="tech-box">
+          <img src="tbd" />
+          <p>TypeScript</p>
+        </div>
+        <div className="tech-box">
+          <img src="tbd" />
+          <p>Go</p>
+        </div>
+        <div className="tech-box">
+          <img src="tbd" />
+          <p>Solidity</p>
+        </div>
+        <div className="tech-box">
+          <img src="tbd" />
+          <p>Docker</p>
+        </div>
+        <div className="tech-box">
+          <img src="tbd" />
+          <p>Kubernetes</p>
+        </div>
+        <h4>Experience With</h4>
+        <div className="tech-box">
+          <img src="tbd" />
+          <p>Figma</p>
+        </div>
+        <div className="tech-box">
+          <img src="tbd" />
+          <p>Shopify</p>
+        </div>
+        <div className="tech-box">
+          <img src="tbd" />
+          <p>WordPress</p>
+        </div>
+        <div className="tech-box">
+          <img src="tbd" />
+          <p>Contentful</p>
+        </div>
+        <div className="tech-box">
+          <img src="tbd" />
+          <p>JIRA</p>
+        </div>
+        <div className="tech-box">
+          <img src="tbd" />
+          <p>Linear</p>
+        </div>
+      </div>
+    </>
+  );
 }
